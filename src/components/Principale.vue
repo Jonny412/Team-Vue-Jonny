3fcf1e9bd2add31ed8aba792ec54920ee35b58e7
Jonny412 <giovanni.micelli@hotmail.com>
2021-06-30 14:25:57 +0200

    newref

--- src/components/Principale.vue
+++ src/components/Principale.vue
@@ -1,22 +1,22 @@
 <template>
-<div id="team">
-    <Destinazione />
-    <PannelloMosart />
-    <DebugPannel />
-</div>
+<div id="team" class="row">
+    <Destinazione class="col-6"/>
+    <PannelloMosart class="col-3"/>
+    <DebugPanel class="col-3"/>
+    </div>
 </template>
 
 <script>
 import Destinazione from './Destinazione'
 import PannelloMosart from './PannelloMosart'
-import DebugPannel from './DebugPannel'
+import DebugPanel from './DebugPanel'
 
 export default {
   name: 'team',
   components: {
     Destinazione,
     PannelloMosart,
-    DebugPannel
+    DebugPanel
 
 
 
