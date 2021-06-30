73047742de363b194935325a155c5acb3c8c03ae
Jonny412 <giovanni.micelli@hotmail.com>
2021-06-30 16:01:59 +0200

    aggConVuetify&Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-     <Principale />
+    <Principale />
   </div>
 </template>
-
 <script>
 import Principale from './components/Principale'
-
 export default {
-  name: 'App',
+  name: 'app',
   components: {
     Principale
-
-  }
+  } 
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+font-family: 'Avenir', Helvetica, Arial, sans-serif;
+-webkit-font-smoothing: antialiased;
+-moz-osx-font-smoothing: grayscale;
+text-align: center;
+color: #2c3e50;
+margin-top: 60px;
 }
 </style>
--- src/components/Principale.vue
+++ src/components/Principale.vue
@@ -1,28 +1,27 @@
 <template>
-<div id="team" class="row">
-    <Destinazione class="col-6"/>
-    <PannelloMosart class="col-3"/>
-    <DebugPanel class="col-3"/>
+  <div class="container">
+    <div class="row">
+      <Destinazione class="col-6"/>
+      <PannelloMosart class="col-3" />
+      <DebugPanel class="col-3"/>
     </div>
+    <div class="d-flex justify-content-center my-2">
+      <button class="btn btn-primary">Click</button>
+    </div>
+  </div>
 </template>
-
 <script>
 import Destinazione from './Destinazione'
 import PannelloMosart from './PannelloMosart'
 import DebugPanel from './DebugPanel'
-
 export default {
   name: 'team',
   components: {
     Destinazione,
     PannelloMosart,
     DebugPanel
-
-
-
   }
 }
 </script>
-
-<style>
+<style scoped>
 </style>
